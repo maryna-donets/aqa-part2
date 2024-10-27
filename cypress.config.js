7a6936df85182ad1739831c27533baf992435cc9
MarynaDonets <kotlyarmv@gmail.com>
2024-10-27 13:43:57 +0200

    cyUI

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    viewportWidth: 1280,
-  viewportHeight: 800
-  },
+	e2e: {
+		chromeWebSecurity: false,
+		viewportWidth: 1280,
+		viewportHeight: 800,
+		defaultCommandTimeout: 10000,
+	},
 });
